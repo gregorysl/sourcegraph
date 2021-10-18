a080d1e556c25c51063f1df0ee93e2716b0ad0ea
Jean-Hadrien Chabran <jh@chabran.fr>
2021-10-18 15:26:23 +0200

    :boar: :lip_stick:

--- enterprise/dev/upload-build-logs.sh
+++ enterprise/dev/upload-build-logs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
+echo "--- building sg"
 pushd dev/sg
 go build -o ../../ci_sg -ldflags "-X main.BuildCommit=$BUILDKITE_COMMIT" -mod=mod .
 popd
 
+echo "--- uploading logs if build failed"
 ./ci_sg ci logs --out=$LOKI_URL --state="failed"
