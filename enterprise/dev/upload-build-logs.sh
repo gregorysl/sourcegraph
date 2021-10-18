adcd8f968efc7b914f4dcb5fdbfecd7b268a946a
Jean-Hadrien Chabran <jh@chabran.fr>
2021-10-18 17:17:26 +0200

    :boar: Bob ross this thing

--- enterprise/dev/upload-build-logs.sh
+++ enterprise/dev/upload-build-logs.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+function usage {
+  cat <<EOF
+Usage: upload-build-logs.sh
+
+Upload a buildkite build result in Loki.
+
+Requires:
+- \$BUILDKITE_COMMIT
+- \$LOKI_URL
+EOF
+}
+
+if [ "$1" == "-h" ]; then
+  usage
+  exit 1
+fi
+
 set -euo pipefail
+
 cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
 echo "--- :go: Building sg"
